5457e3712a4b7b153251914fc8cbf3333917ccf8
medchakh <medchakhman@gmail.com>
2022-10-13 22:03:11 +0000

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - is Entry point
+ *
+ * The function will return  a string
+ * Return: Always 0 
+ */
 int main(void)
 {
 char *a = "\"Programming is like building a multilingual puzzle";
