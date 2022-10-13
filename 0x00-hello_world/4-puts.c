53eca726d8f61493ded424a224c29eddcb2f36be
medchakh <medchakhman@gmail.com>
2022-10-13 22:02:00 +0000

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+char *a = "\"Programming is like building a multilingual puzzle";
+
+puts(a);
+
+return (0);
+
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -2,3 +2,4 @@
 [1-complier](./1-compiler).
 [2-assembler](./2-assembler).
 [3-name](./3-name).
+[4-puts.c](./4-puts.c).
