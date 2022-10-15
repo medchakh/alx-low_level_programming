8bd56ae6ecdb9b793985c658ccbccf7dc1fa3a52
medchakh <medchakhman@gmail.com>
2022-10-15 11:53:46 +0000

    alphabets 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch, new_line;
+
+new_line = '\n';
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar(new_line);
+return (0);
+}
