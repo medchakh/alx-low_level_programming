fb44e4347dffc34859e2263e4faf697716a06e94
medchakh <medchakhman@gmail.com>
2022-10-18 21:44:37 +0000

    2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,14 @@
 
 int main(void)
 {
-	_putchar('_putchar');
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
 	_putchar('\n');
+
 	return (0);
 }
