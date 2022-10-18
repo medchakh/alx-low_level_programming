8933b2090e727ee14bfdd846b0ca521aee02900a
medchakh <medchakhman@gmail.com>
2022-10-18 19:28:23 +0000

    002 functions and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -6,9 +7,15 @@
  */
 int main(void)
 {
-	char ch = "_putchar";
-
-	_putchar(ch)
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
