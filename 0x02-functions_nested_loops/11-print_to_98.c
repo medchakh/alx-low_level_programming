44a5e40949b216661b2ef6c791b2d1979347e0ff
medchakh <medchakhman@gmail.com>
2022-10-18 22:23:50 +0000

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - print all integer numbers, begin from input number stop at 98.
+ * @n: the input number
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else if (n > 98)
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+		printf("%d\n", 98);
+}
