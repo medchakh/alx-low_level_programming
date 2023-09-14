cdd3bec909790eecd6d1da90081f90b6ad6ee593
Mohamed Chakhman <medchakhman@gmail.com>
2023-09-14 21:12:31 +0100

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,8 @@
 #include "main.h"
 
 /**
-  * print_most_numbers - Print the numbers since 0 up to 9
-  *
-  * Description: Prints the numbers excluding 2 and 4
-  *
-  * Return: The numbers since 0 up to 9
-  */
+ * print_most_numbers - Prints the numbers from 0 to 9 excluding 2 and 4
+ */
 void print_most_numbers(void)
 {
 	int x = 0;
