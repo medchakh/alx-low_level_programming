999a918a41f542fa8981ee37687a52cde2b74cad
Mohamed Chakhman <medchakhman@gmail.com>
2023-09-14 20:41:43 +0100

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,5 @@
-#include "main.h"
-#include <stdio.h>
+#include <ctype.h>
 
-/**
- * print_numbers -  a function that prints the numbers, from 0 to 9.
- * no variable input
- *
- * Ruturn: void
- */
- 
- int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+int _isupper(int c) {
+    return isupper(c) ? 1 : 0;
 }
