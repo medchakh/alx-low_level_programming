9659bd3534b8d4a3991d552b81954510f0a9aeee
medchakh <medchakhman@gmail.com>
2023-10-19 17:23:26 +0000

    Done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,11 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
-
-    while (h)
-    {
-        count++;
-        h = h->next;
-    }
-
-    return count;
+size_t n = 0;
+while (h)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
