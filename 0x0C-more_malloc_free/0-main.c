2e479151dc055fc018f5cd01df17b377132be096
medchakh <medchakhman@gmail.com>
2023-11-23 11:18:13 +0000

    0x0C

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -1,30 +1,20 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include "main.h"
 
 /**
- * main - contains all files
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
  *
- * Return: Always 0
+ * Return: a pointer to the allocated memory
  */
-int main(void)
+void *malloc_checked(unsigned int b)
 {
-char *c;
-int *i;
-float *f;
-double *d;
-c = malloc_checked(sizeof(char) * 1024);
-printf("%p\n", (void *)c);
-i = malloc_checked(sizeof(int) * 402);
-printf("%p\n", (void *)i);
-f = malloc_checked(sizeof(float) * 100000000);
-printf("%p\n", (void *)f);
-d = malloc_checked(INT_MAX);
-printf("%p\n", (void *)d);
-free(c);
-free(i);
-free(f);
-free(d);
-return (0);
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
